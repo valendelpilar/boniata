bb859fe31cbdb097851d1e2eccd14961485bff28
Valentina <valencondde@gmail.com>
2022-01-03 19:20:24 -0300

    agregue cantidad total

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 fetch('js/productos.json')
-
+    
     .then(response => response.json())
     .then(data => {
 
@@ -15,7 +15,7 @@ fetch('js/productos.json')
                             <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
                             <p class="card-text descripcion">${producto.descripcion} </p>
                             <p class="card-text precio">$${producto.precio} </p>
-                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito</button>            
+                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>            
                         </div>
                     </div>
                 `
@@ -34,7 +34,7 @@ fetch('js/productos.json')
                             <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
                             <p class="card-text descripcion">${producto.descripcion} </p>
                             <p class="card-text precio">$${producto.precio} </p>
-                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito</button>
+                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>
                         </div>
                     </div>
                 `
@@ -45,6 +45,8 @@ fetch('js/productos.json')
 
         //AGREGAR PRODUCTOS AL CARRITO
         let carrito = []
+        let total = 0
+
         data.forEach((producto, indice) => {
             if (localStorage.length > 0) {
                 carrito = JSON.parse(localStorage.getItem('carrito'))
@@ -59,8 +61,13 @@ fetch('js/productos.json')
                     producto.cantidad++
                     carrito.push(producto)
                 }
+                
                 localStorage.setItem('carrito', JSON.stringify(carrito))
+                total = total + producto.precio
+                localStorage.setItem('total',JSON.stringify(total))
             })
+
+
         })
 
 
@@ -69,104 +76,113 @@ fetch('js/productos.json')
 
 
 
-//MOSTRAR CARRITO
-let carrito_compras_html = document.getElementById('modal-body')
-
-document.getElementById('boton_carrito').addEventListener('click', () => {
-
-    let productos_parseados = JSON.parse(localStorage.getItem('carrito'))
-    carrito_compras_html.innerHTML = ''
-    if (productos_parseados != null) {
-        productos_parseados.forEach((producto, indice) => {
-            carrito_compras_html.innerHTML += `
+        //MOSTRAR CARRITO
+        let carrito_compras_html = document.getElementById('modal-body')
+        
+        
+        document.getElementById('boton_carrito').addEventListener('click', () => {
             
-                <div class="card m-2" style="width: 150px; height: auto" id="producto_carrito${producto.id}">
-                    <div class="card-body card_productos">
+            let productos_parseados = JSON.parse(localStorage.getItem('carrito'))
+            let total = JSON.parse(localStorage.getItem('total'))
+            carrito_compras_html.innerHTML = ''
+            if (productos_parseados.length > 0) {
+                productos_parseados.forEach((producto, indice) => {
+                    carrito_compras_html.innerHTML += `
+            
+                <div class="card m-2 card_carrito" style="width: 150px; height: auto; border:white" id="producto_carrito${producto.id}">
+                    <div class="card-body card_productos_carrito">
                     <img src="multimedia/producto${producto.id}.jpg" class="card-img-top imagen_carrito" alt="imagen_producto">
-                        <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
-                        <p class="card-text precio">$${producto.precio} </p>
-                        <p class="card-text precio">Cantidad: ${producto.cantidad} </p>
-                        <button type="button" class="btn btn-outline-danger" id="boton_borrar_producto${producto.id}">Borrar producto</button>
-                        <button type="button" class="btn btn-secondary" id="boton_subir_cantidad${producto.id}">sumar</button>
-                        <button type="button" class="btn btn-secondary" id="boton_bajar_cantidad${producto.id}">restar</button>
+                        <div class="informacion_producto">
+                            <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
+                            <p class="card-text precio">$${producto.precio} </p>
+                            <p class="card-text cantidad">cantidad: ${producto.cantidad} </p>
+                            <div class="botones d-flex">
+                            <button type="button" class="btn btn-outline-secondary boton_cantidad" id="boton_bajar_cantidad${producto.id}">-</button>
+                            <button type="button" class="btn btn-outline-primary boton_cantidad" id="boton_subir_cantidad${producto.id}">+</button>
+                            <button type="button" class="btn btn-outline-danger boton_borrar" id="boton_borrar_producto${producto.id}">Borrar producto<i class="fas fa-trash-alt ms-2"></i></button>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
+                <h3>Total: $${total}</h3>
+                
+
             `
-        })
-    }
-    else {
-        carrito_compras_html.innerHTML += `
+                })
+                
+            }
+            else {
+                carrito_compras_html.innerHTML += `
             <h4 class="d-flex justify-content-center mt-4">no hay productos</h4>
         `
-    }
-
-    //borrar productos del carrito
-        carrito.forEach((producto, indice) => {
-            document.getElementById(`boton_borrar_producto${producto.id}`).addEventListener('click', () => {
-                event.preventDefault()
-                let found = carrito.find(productoCarrito => productoCarrito.id === producto.id)
-                if (found != null && found.cantidad > 1) {
-                    found.cantidad--
-                }
-                else {
-                    let lugar = carrito.findIndex(productoCarrito => productoCarrito.id === producto.id)
-                    carrito.splice(lugar,1)
-                }
-                localStorage.setItem('carrito', JSON.stringify(carrito))
-            })
-        })
-
-
-
-        carrito.forEach((producto, indice) => {
-            document.getElementById(`boton_bajar_cantidad${producto.id}`).addEventListener('click', () => {
-                event.preventDefault()
-                let found = carrito.find(productoCarrito => productoCarrito.id === producto.id)
-                if (found != null && found.cantidad > 1) {
-                    found.cantidad--
-                }
-                else {
-                    let lugar = carrito.findIndex(productoCarrito => productoCarrito.id === producto.id)
-                    carrito.splice(lugar,1)
-                }
-                localStorage.setItem('carrito', JSON.stringify(carrito))
-            })
-        })
-    
-    
-    
-        carrito.forEach((producto, indice) => {
-            if (localStorage.length > 0) {
-                carrito = JSON.parse(localStorage.getItem('carrito'))
             }
-            document.getElementById(`boton_subir_cantidad${producto.id}`).addEventListener('click', () => {
-                event.preventDefault()
-                let found = carrito.find(productoCarrito => productoCarrito.id === producto.id)
-                if (found != null && found.cantidad > 0) {
-                    found.cantidad++
+
+            //borrar productos del carrito
+            carrito.forEach((producto, indice) => {
+                document.getElementById(`boton_borrar_producto${producto.id}`).addEventListener('click', () => {
+                    event.preventDefault()
+                    let lugar = carrito.findIndex(productoCarrito => productoCarrito.id === producto.id)
+                    carrito.splice(lugar, 1)
+                    localStorage.setItem('carrito', JSON.stringify(carrito))
+                    total = total - (producto.precio * producto.cantidad)
+                    localStorage.setItem('total',JSON.stringify(total))
+                    producto.cantidad = 0
+
+                })
+            })
+
+
+            //bajar cantidad producto
+            carrito.forEach((producto, indice) => {
+                document.getElementById(`boton_bajar_cantidad${producto.id}`).addEventListener('click', () => {
+                    event.preventDefault()
+                    let found = carrito.find(productoCarrito => productoCarrito.id === producto.id)
+                    if (found != null && found.cantidad > 1) {
+                        found.cantidad--
+                    }
+                    else {
+                        let lugar = carrito.findIndex(productoCarrito => productoCarrito.id === producto.id)
+                        carrito.splice(lugar, 1)
+                    }
+                    localStorage.setItem('carrito', JSON.stringify(carrito))
+                    total = total - producto.precio
+                    localStorage.setItem('total',JSON.stringify(total))
+                })
+            })
+
+
+            //subir cantidad producto
+            carrito.forEach((producto, indice) => {
+                if (localStorage.length > 0) {
+                    carrito = JSON.parse(localStorage.getItem('carrito'))
                 }
-                localStorage.setItem('carrito', JSON.stringify(carrito))
+                document.getElementById(`boton_subir_cantidad${producto.id}`).addEventListener('click', () => {
+                    event.preventDefault()
+                    let found = carrito.find(productoCarrito => productoCarrito.id === producto.id)
+                    if (found != null && found.cantidad > 0) {
+                        found.cantidad++
+                    }
+                    localStorage.setItem('carrito', JSON.stringify(carrito))
+                    total = total + producto.precio
+                    localStorage.setItem('total',JSON.stringify(total))
+                })
             })
         })
+
+
+
     })
 
 
-
-
-
+    
     
     
 
-})
 
 
 
-
-
-
-    
-
-.catch(error => console.log(error))
+    .catch(error => console.log(error))
 
 
 
@@ -176,7 +192,7 @@ document.getElementById('boton_carrito').addEventListener('click', () => {
 let boton_finalizar_compra_html = document.getElementById('boton_finalizar_compra')
 boton_finalizar_compra_html.addEventListener('click', () => {
     localStorage.setItem('carrito', JSON.stringify([]))
-    alert("Gracias por su compra!")
+    swal.fire("Gracias por su compra!", "Los productos seran enviados en la brevedad", "success")
 })
 
 
