ce923d8dc56fd0a07a1ae82421fd534311e8f6b4
Valentina <valencondde@gmail.com>
2022-01-20 17:31:12 -0300

    quite cards

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ fetch('js/productos.json')
     .then(response => response.json())
     .then(data => {
 
-
         let carrito = []
         let total = 0
         let alfajores_html = document.getElementById('div_alfajores')
@@ -24,7 +23,6 @@ fetch('js/productos.json')
         const traer_local_storage = () => {
             let carrito = JSON.parse(localStorage.getItem('carrito'))
             let total = JSON.parse(localStorage.getItem('total'))
-
             return { carrito, total }
         }
 
@@ -33,14 +31,12 @@ fetch('js/productos.json')
         data.map((producto) => {
             if (producto.tipo == "alfajor") {
                 alfajores_html.innerHTML += `
-                    <div class="card m-2 " id="producto${producto.id}" style="width: 18rem;">
-                        <img src="multimedia/producto${producto.id}.jpg" class="card-img-top imagen" alt="...">
-                        <div class="card-body card_productos">
-                            <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
-                            <p class="card-text descripcion">${producto.descripcion} </p>
-                            <p class="card-text precio">$${producto.precio} </p>
-                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>            
-                        </div>
+                    <div class="d-flex p-2 m-2 flex-md-column bg-danger bg-gradient bg-opacity-10 justify-content-center text-center border border-danger card_font" id="producto${producto.id}" style="width: 18rem;">
+                        <img src="multimedia/producto${producto.id}.jpg" class="imagen" alt="imagen">
+                        <p>${producto.nombre} - <strong>${producto.marca}</strong> </p>
+                        <p class="fs-6">${producto.descripcion} </p>
+                        <p class="precio">$${producto.precio} </p>
+                        <button type="button" class="btn btn-dark bg-gradient boton_carrito_producto" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>            
                     </div>
                 `
             }
@@ -51,14 +47,12 @@ fetch('js/productos.json')
         data.map((producto) => {
             if (producto.tipo == "chocolate") {
                 chocolates_html.innerHTML += `
-                    <div class="card m-2 " id="producto${producto.id}" style="width: 18rem;">
-                        <img src="multimedia/producto${producto.id}.jpg" class="card-img-top imagen" alt="...">
-                        <div class="card-body card_productos">
-                            <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
-                            <p class="card-text descripcion">${producto.descripcion} </p>
-                            <p class="card-text precio">$${producto.precio} </p>
-                            <button type="button" class="btn btn-outline-success" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>
-                        </div>
+                    <div class="d-flex p-2 m-2 flex-md-column bg-danger bg-gradient bg-opacity-10 justify-content-center border border-danger text-center card_font" id="producto${producto.id}" style="width: 18rem;">
+                        <img src="multimedia/producto${producto.id}.jpg" class="imagen" alt="imagen">
+                        <p class=>${producto.nombre} - <strong>${producto.marca}</strong> </p>
+                        <p class=>${producto.descripcion} </p>
+                        <p class="precio">$${producto.precio} </p>
+                        <button type="button" class="btn btn-dark bg-gradient boton_carrito_producto" id="boton_carrito_producto${producto.id}">Agregar al carrito<i class="fas fa-shopping-basket ps-2"></i></button>            
                     </div>
                 `
             }
@@ -85,6 +79,7 @@ fetch('js/productos.json')
             let index = carrito.findIndex(productoCarrito => productoCarrito.id == producto.id)
 
             producto.cantidad++
+
             if (index >= 0) {
                 carrito.splice(index, 1)
                 carrito.push(producto)
@@ -109,58 +104,46 @@ fetch('js/productos.json')
 
                 carrito.map((producto) => {
                     carrito_compras_html.innerHTML += `
-            
-                <div class="card m-2 card_carrito" style="width: 150px; height: auto; border:white" id="producto_carrito${producto.id}">
-                    <div class="card-body card_productos_carrito">
-                    <img src="multimedia/producto${producto.id}.jpg" class="card-img-top imagen_carrito" alt="imagen_producto">
-                        <div class="informacion_producto">
-                            <p class="card-text nombre">${producto.nombre} - <strong>${producto.marca}</strong> </p>
-                            <p class="card-text precio">$${producto.precio} </p>
-                            <p class="card-text cantidad">cantidad: ${producto.cantidad} </p>
-                            <div class="botones d-flex">
-                            <button type="button" class="btn btn-outline-secondary boton_cantidad" id="boton_bajar_cantidad${producto.id}">-</button>
-                            <button type="button" class="btn btn-outline-primary boton_cantidad" id="boton_subir_cantidad${producto.id}">+</button>
-                            <button type="button" class="btn btn-outline-danger boton_borrar" id="boton_borrar_producto${producto.id}">Borrar producto<i class="fas fa-trash-alt ms-2"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>                
-
-            `
+                        <div class="card_font d-flex text-center" style="width: 150px; height: auto; border:white" id="producto_carrito${producto.id}">
+                            <img src="multimedia/producto${producto.id}.jpg" class="imagen_carrito me-5" alt="imagen_producto">
+                                <div class="informacion_producto">
+                                    <p>${producto.nombre} - <strong>${producto.marca}</strong> </p>
+                                    <p class="precio">$${producto.precio} </p>
+                                    <p>cantidad: ${producto.cantidad} </p>
+                                        <div class="d-flex">
+                                            <button type="button" class="btn btn-outline-secondary me-3" id="boton_bajar_cantidad${producto.id}">-</button>
+                                            <button type="button" class="btn btn-outline-primary ms-2 me-3" id="boton_subir_cantidad${producto.id}">+</button>
+                                            <button type="button" class="btn btn-outline-danger boton_borrar" id="boton_borrar_producto${producto.id}">Borrar producto<i class="fas fa-trash-alt ms-2"></i></button>
+                                        </div>
+                                </div>
+                        </div>                
+                    `
                 })
 
                 carrito_compras_html.innerHTML += `
-                <h3>Total: $${total}</h3>
-                <h6>Salga y entre del carrito para ver el precio actual</h6>
-                
+                    <h3 class="mt-5">Total: $${total}</h3>
+                    <h6>Salga y entre del carrito para ver el precio actual</h6>
                 `
             }
             else {
                 carrito_compras_html.innerHTML += `
-            <h4 class="d-flex justify-content-center mt-4">no hay productos</h4>
-            `
+                    <h4 class="d-flex justify-content-center mt-4">no hay productos</h4>
+                `
             }
-
-
-
-
-
-
 
 
             //borrar productos del carrito
             carrito.map((producto) => {
 
                 document.getElementById(`boton_borrar_producto${producto.id}`).onclick = () => {
-
                     event.preventDefault()
 
                     let index = carrito.findIndex(productoCarrito => productoCarrito.id == producto.id)
                     borrar_agregar_producto(producto.cantidad, producto, index)
                     agregar_local_storage(carrito, total)
                     alert('Producto borrado correctamente!')
-
                 }
+
             })
 
 
@@ -173,8 +156,6 @@ fetch('js/productos.json')
                     borrar_agregar_producto(1, producto, index)
                     agregar_local_storage(carrito, total)
                     alert(`Producto borrado correctamente! Cantidad actual: ${producto.cantidad}`)
-
-
                 }
             })
 
@@ -188,20 +169,16 @@ fetch('js/productos.json')
                     borrar_agregar_producto(-1, producto, index)
                     agregar_local_storage(carrito, total)
                     alert(`Producto agregado correctamente! Cantidad actual: ${producto.cantidad}`)
-
-
                 }
             })
-
 
 
             //funcion para borrar y agregar productos
             const borrar_agregar_producto = (cantidad, producto, index) => {
                 total = total - producto.precio * cantidad
 
-                if (producto.cantidad == 1 || producto.cantidad == cantidad) {
+                if (producto.cantidad == 1 || producto.cantidad == cantidad)
                     carrito.splice(index, 1)
-                }
 
                 producto.cantidad = producto.cantidad - cantidad
             }
@@ -210,13 +187,12 @@ fetch('js/productos.json')
 
         //FINALIZA LA COMPRA
         boton_finalizar_compra_html.onclick = () => {
-            let {carrito,total} = traer_local_storage()
+            let { carrito, total } = traer_local_storage()
 
             swal.fire("Gracias por su compra!", `Precio final: $${total}`, "success")
 
-            for (let i = 0; i < carrito.length; i++) {
+            for (let i = 0; i < carrito.length; i++)
                 carrito[i].cantidad = 0
-            }
 
             carrito = []
             total = 0
@@ -227,6 +203,4 @@ fetch('js/productos.json')
 
     })
 
-
     .catch(error => console.log(error))
-
