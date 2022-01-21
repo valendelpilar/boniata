00f58374ccd01a6788c7b8e499c531745a00b744
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2022-01-20 22:15:53 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ fetch('js/productos.json')
 
         //FINALIZA LA COMPRA
         boton_finalizar_compra_html.onclick = () => {
-            let { carrito, total } = traer_local_storage()
+           // let { carrito, total } = traer_local_storage()
 
             swal.fire("Gracias por su compra!", `Precio final: $${total}`, "success")
 
